9e502cb78cdb68b407850f41166f243966126399
Baptiste Menant <ba.menant@gmail.com>
2020-02-02 15:09:19 +0100

    on relance le tout

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -2,30 +2,18 @@
   <div id="app" class="small container">
     
     <h1>All</h1>
-    <Cart />
-    <ul>
-      <li class="menu-horizontal">All</li>
-      <li class="menu-horizontal">Drinks</li>
-      <li class="menu-horizontal">Desserts</li>
-    </ul>
+
     <h2>Drinks</h2>
-    <drinks-table />
     <h2>Desserts</h2>
-    <desserts-table />
   </div>
 </template>
 
 <script>
-import DrinksTable from '@/components/DrinksTable.vue'
-import DessertsTable from '@/components/DessertsTable.vue'
-import Cart from '@/components/Cart.vue'
 
 export default {
   name: 'app',
   components: {
-    DrinksTable,
-    DessertsTable,
-    Cart,
+
   }
 }
 </script>
